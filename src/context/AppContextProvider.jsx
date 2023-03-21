b4830c50c1aebf43f6d958cee2cff0029934c30e
Valentino Simon <cucuxlife@gmail.com>
2023-03-21 00:56:26 -0300

    agregado dispatch y sus llamados , ShoppingReducer

--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -1,12 +1,15 @@
 import { useReducer } from 'react'
+import { shoppingInitialState , shoppingReducer } from '../reducers/ShoppingReducer'
 import { AppContext } from './AppContext'
 
 
 export const AppContextProvider = ({ children }) => {
-    // const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState)
+
+    const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState)      
+
     return (
         
-        <AppContext.Provider value={{/*{state, dispatch}*/}}>
+        <AppContext.Provider value={{state, dispatch}}>
             { children }
         </AppContext.Provider>
     )
