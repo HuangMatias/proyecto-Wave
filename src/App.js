4883516ef9eda4d6b2f2149403f65322fbfc625e
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2023-03-22 23:13:53 -0300

    Adaptacion de context y reducers. Cambios en client api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { useContext, useEffect } from 'react';
 
 import { getProducts } from './api/products';
+import Homepage from './components/pages/Homepage';
 import { AppContext } from './context/AppContext';
+import {TYPES} from './reducers/actions/actions_shopping'
 
 function App() {
     const {dispatch} = useContext(AppContext)
     useEffect(() => {
+
         getProducts().then((products) => {
-            dispatch({type: 'FILL_PRODUCT_LIST', payload: products})
+            dispatch({type: TYPES.FILL_PRODUCT_LIST, payload: products})
     
         });
     
@@ -16,7 +19,7 @@ function App() {
 
     return (
         <>
-            <div></div>
+            <Homepage/>
         </>
     );
 }
--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/v1',
+    baseURL: 'https://wave-api.onrender.com/api/v1',
+    // baseURL: 'http://127.0.0.1:8000/api/v1',
 });
