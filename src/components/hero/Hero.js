0f834dd46477667ecefb0a23f4142e7e9b18f0cc
Marcelo Bionti <marcelo@hotmail.com>
2023-04-10 21:04:56 -0300

    cambio nombre hero

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+import heroimg from './assets/imagenhero.webp'
+
+  const HeroComponet = () => {
+  return (
+    <div className='container mt-3'>
+      <div className='row'>
+        Wave Siempres con vos...
+        <img className='img-fluid w-100' src={heroimg} alt='imagen hero' />
+
+        </div> 
+      HeroComponet</div>
+  );
+}
+
+export default HeroComponet;
