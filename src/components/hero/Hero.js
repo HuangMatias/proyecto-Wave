8d2f9cb9e1f62da41bc070439702c78f128f94fe
Marcelo Bionti <marcelo@hotmail.com>
2023-04-17 14:37:57 -0300

    arreglo hero

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,13 +3,17 @@
 import React from "react";
 import heroimg from "../../assets/imagenhero.jpg"
 
+
   const Hero = () => {
   return (
     <div className='container mt-3'>
       <div className='row'>
-        Wave Siempres con vos...
+        <div className='hero'>
+          Wave Siempres con vos...
+                
+        
         <img className='img-fluid w-100' src={heroimg} alt='imagen hero' />
-
+        </div> 
       </div> 
     </div>
   );
